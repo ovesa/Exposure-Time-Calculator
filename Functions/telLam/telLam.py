b9e06d80aa2f4019743c0c1208c6e591e92e963e
Farhanul Hasan <farhasan@nmsu.edu>
2019-10-04 19:35:56 +0000

    Update telLam.py

--- Functions/telLam/telLam.py
+++ Functions/telLam/telLam.py
@@ -3,16 +3,17 @@
 import numpy as np
 
 def telLam(d_p,d_ob,R):
+    
 	''' calculates telescope throughput given:
-	d_p = telescope primary diameter, meters
-	d_p = diameter of central obstruction, meters
-	R = reflectance (of Aluminum)
+	d_p = telescope primary diameter, cm
+	d_p = diameter of central obstruction, cm
 	'''
-	R_p = d_p/2
-	R_ob = d_ob/2
-	A_p = np.pi * R_p**2
-	A_ob = np.pi * R_ob**2
+	
+	r_p = d_p/2
+	r_ob = d_ob/2
+	A_p = np.pi * r_p**2
+	A_ob = np.pi * r_ob**2
 
-	tp = (A_p - A_ob) * (R**3) #throughput = effective collecting area * R**3
+	tp = (A_p - A_ob) * (R**3) #throughput = effective collecting area * R**3, in cm^2
 
 	return tp
