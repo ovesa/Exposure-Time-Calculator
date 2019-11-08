7bf7ea5d29a786ec3756f0f8499a1b67cc7f59c7
Matthew Varakian <mvarakian9@gmail.com>
2019-11-08 20:55:18 +0000

    Fixed exit equation

--- Functions/solveForExp/solve_expT.py
+++ Functions/solveForExp/solve_expT.py
@@ -4,15 +4,15 @@ import numpy as np
 
 def calc_expT(SN,flux,back,seeing,tel):
 
-	''' returns exposure time given inputs:
-		S/N, 
-		photon flux from source (counts/s/cm^2/A), 
-		background SB (counts/s/m^2/micron/arcsec^2), 
-		seeing (arcsec),
-		telescope collecting area (cm^2)
-	'''
-	back_area = np.pi * seeing**2 #background area
-	bgr = back*1e8 #convert background SB to units of counts/s/cm^2/A/arcsec^2
-	count = flux * 1e4
-	expT = (((bgr*back_area*tel) + count)/count**2)*(SN**2) #from noise equation
-	return expT
+    ''' returns exposure time given inputs:
+    S/N,
+    photon flux from source (counts/s/cm^2/um),
+    background SB (counts/s/m^2/arcsec^2),
+    seeing (arcsec),
+    telescope collecting area (cm^2)
+    '''
+    back_area = np.pi * seeing**2 #background area
+    bgr = back#*(10**4) #convert background SB to units of counts/s/cm^2/arcsec^2
+    #expT = (((bgr*back_area*tel) + flux)/flux**2)*SN**2 #from noise equation
+    expT = ((SN/(flux*tel**.5))*(flux+back_area*bgr)**.5)**2
+    return expT
