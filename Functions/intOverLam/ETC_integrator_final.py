97ae5c2aa91b00e59ac69c76a7dfb97434c8f548
ksand017 <ksand017@odu.edu>
2019-11-21 20:22:47 +0000

    Update ETC_integrator_final.py; moved the doc-string into the function

--- Functions/intOverLam/ETC_integrator_final.py
+++ Functions/intOverLam/ETC_integrator_final.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-"""
-Created on Tue. Oct, 11 2019
 
-@author: kellysanderson
-===============================================================================
-Simpson's Rule for Integration of the Count Equation:
-    
-    Function that approximates the integral of a the count equation
-    Input:
-        All inputs should be functions of lambda, 
-        lambda_vals: single value or array of lambda values as indicated by user input
-        phot_flux: source flux in units of photons per area per time
-        atm_e: single value of array of atm transmission values corresponding to user input wavelengths
-        tel_e: single value or array of telescope transmission values corresponding to user input wavlengths
-        filt_e: single value or array of filter transmission values corresponding to user input wavelengths
-        ints_e: single value or array of instruments transmission values corresponding to user input wavelengths
-        det_e: single value or array of detector transmission values corresponding to user input wavelengths
-    Output:
-        approximate integral of function 
-===============================================================================
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -28,7 +8,24 @@ from astropy.table import Table, Column
 from scipy import integrate
 
 def GetSprime(lambda_val, phot_flux, atm_e, tel_e, inst_e, filt_e, det_e):
-
+    """
+    ===============================================================================
+    "Integration" of the Count Equation:
+        
+        Function that approximates the integral of a the count equation
+        Input:
+            All inputs should be functions of lambda, 
+            lambda_vals: single value or array of lambda values as indicated by user input
+            phot_flux: source flux in units of photons per area per time
+            atm_e: single value of array of atm transmission values corresponding to user input wavelengths
+            tel_e: single value or array of telescope transmission values corresponding to user input wavlengths
+            filt_e: single value or array of filter transmission values corresponding to user input wavelengths
+            ints_e: single value or array of instruments transmission values corresponding to user input wavelengths
+            det_e: single value or array of detector transmission values corresponding to user input wavelengths
+        Output:
+            approximate integral of function 
+    ===============================================================================
+    """
     #if type(lambda_val) is float or type(lambda_val) is int:
     Y = phot_flux*atm_e*tel_e*inst_e*filt_e*det_e*lambda_val
     return Y
